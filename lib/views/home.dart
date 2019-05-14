ff1c3aa0a3eb18f3c3ce2c4ecfd31c2be6557e26
ledfuse <ledfuse@users.noreply.github.com>
2019-05-14 20:06:02 +0200

    Displaying information on the Home page

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,8 +1,7 @@
-import 'dart:convert';
-
 import "package:flutter/material.dart";
 import 'package:vocdoni/constants/colors.dart';
 import 'package:vocdoni/util/singletons.dart';
+// import 'dart:convert';
 import '../lang/index.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -13,82 +12,94 @@ class HomeScreen extends StatelessWidget {
           title: Text("Vocdoni"),
           backgroundColor: mainBackgroundColor,
         ),
-        drawer: Drawer(
-          child: ListView(
-            // Important: Remove any padding from the ListView.
-            padding: EdgeInsets.zero,
-            children: <Widget>[
-              DrawerHeader(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text('My Identity',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 22,
-                          letterSpacing: 1,
-                        )),
-                    Text('0x12341234',
-                        style: TextStyle(
-                          color: Colors.white,
-                        )),
-                  ],
-                ),
-                decoration: BoxDecoration(
-                  color: Colors.blue,
-                ),
-              ),
-              ListTile(
-                leading: Icon(Icons.person),
-                title: Text('Switch Identity'),
-                onTap: () {
-                  Navigator.pop(context);
-                },
-              ),
-              ListTile(
-                leading: Icon(Icons.home),
-                title: Text('Organization 1'),
-                onTap: () => {},
-              ),
-              ListTile(
-                leading: Icon(Icons.home),
-                title: Text('Organization 2'),
-                onTap: () => {},
-              ),
-              ListTile(
-                leading: Icon(Icons.exit_to_app),
-                title: Text('Logout'),
-                onTap: () {
-                  Navigator.pop(context);
-                  Navigator.pushReplacementNamed(context, "/welcome");
-                },
-              ),
-            ],
+        drawer: HomeDrawer(context),
+        body: HomeBody(context));
+  }
+
+  HomeDrawer(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          DrawerHeader(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('My Identity',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 22,
+                      letterSpacing: 1,
+                    )),
+                Text('0x12341234',
+                    style: TextStyle(
+                      color: Colors.white,
+                    )),
+              ],
+            ),
+            decoration: BoxDecoration(
+              color: mainBackgroundColor,
+            ),
           ),
-        ),
-        body: StreamBuilder(
+          ListTile(
+            leading: Icon(Icons.person),
+            title: Text('Switch Identity'),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.home),
+            title: Text('Organization 1'),
+            onTap: () => {},
+          ),
+          ListTile(
+            leading: Icon(Icons.home),
+            title: Text('Organization 2'),
+            onTap: () => {},
+          ),
+          ListTile(
+            leading: Icon(Icons.exit_to_app),
+            title: Text('Logout'),
+            onTap: () {
+              Navigator.pop(context);
+              Navigator.pushReplacementNamed(context, "/welcome");
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
+  HomeBody(BuildContext context) {
+    return Center(
+      child: StreamBuilder(
           stream: identitiesBloc.stream,
           builder:
               (BuildContext context, AsyncSnapshot<List<Identity>> identities) {
-            return Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Text("HOME SCREEN - IDENTITY - ORGANIZATIONS"),
-                  (identities.data == null)
-                      ? Text("(empty?)")
-                      : Text(identities.data.map((idt) {
-                            if (idt != null)
-                              return idt.alias ?? "";
-                            else
-                              return "---";
-                          }).join(", ") ??
-                          "")
-                ],
-              ),
-            );
-          },
-        ));
+            return StreamBuilder(
+                stream: appStateBloc.stream,
+                builder:
+                    (BuildContext context, AsyncSnapshot<AppState> appState) {
+                  return Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text("HOME SCREEN"),
+                      Text("Available Identities:"),
+                      (identities.data == null)
+                          ? Text("(empty?)")
+                          : Text(identities.data
+                              .map((idt) => idt.alias)
+                              .join(", ")),
+                      Text("\n\nCURRENT ID: ${appState.data.selectedIdentity}"),
+                      Text(identities
+                          .data[appState.data.selectedIdentity].alias),
+                      Text("\nCURRENT ORG: ${appState.data.selectedOrganization}")
+                    ],
+                  );
+                });
+          }),
+    );
   }
 }
