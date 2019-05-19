3292a34e33dbbd95a0774efc99d22492726c97bc
ledfuse <ledfuse@users.noreply.github.com>
2019-05-20 01:12:38 +0200

    Tab routing refactor WIP

--- lib/views/news-feed-tab.dart
+++ lib/views/news-feed-tab.dart
@@ -3,7 +3,7 @@ import 'package:vocdoni/constants/colors.dart';
 import 'package:vocdoni/util/singletons.dart';
 import 'package:vocdoni/widgets/pageTitle.dart';
 import 'package:flutter_html/flutter_html.dart';
-import 'package:url_launcher/url_launcher.dart';
+import 'package:url_launcher/url_launcher.dart'; // TODO: REMOVE
 
 class NewsFeedTab extends StatelessWidget {
   final Organization organization;
--- lib/views/organization-info.dart
+++ lib/views/organization-info.dart
@@ -1,17 +1,14 @@
 import "package:flutter/material.dart";
 import 'package:vocdoni/util/singletons.dart';
-import 'package:vocdoni/views/organization-activity.dart';
+// import 'package:vocdoni/views/organization-activity.dart';
 import 'package:vocdoni/widgets/listItem.dart';
 import 'package:vocdoni/widgets/pageTitle.dart';
 import 'package:vocdoni/widgets/section.dart';
 
-class OrganizationTab extends StatelessWidget {
-  final Organization organization;
-
-  OrganizationTab({this.organization});
-
+class OrganizationInfo extends StatelessWidget {
   @override
   Widget build(context) {
+    final Organization organization = ModalRoute.of(context).settings.arguments;
     if (organization == null) return buildEmptyOrganization(context);
 
     return ListView(
@@ -32,7 +29,7 @@ class OrganizationTab extends StatelessWidget {
           text: "Activity",
           onTap: () {
             Navigator.pushNamed(context, "/organizations/activity",
-                arguments: {"organization": organization});
+                arguments: organization);
           },
         ),
       ],
@@ -42,7 +39,7 @@ class OrganizationTab extends StatelessWidget {
   Widget buildEmptyOrganization(BuildContext ctx) {
     // TODO: UI
     return Center(
-      child: Text("(No organizations)"),
+      child: Text("(No organization)"),
     );
   }
 }
