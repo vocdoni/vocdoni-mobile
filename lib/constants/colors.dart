943a2e7ae17486827f17b975eb46967641e4b622
Xavi <xavings@gmail.com>
2019-05-21 12:27:40 +0200

    Abstracting mnemonic widgets and button

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -17,9 +17,14 @@ final Color titleColor = Color(0xFF000000);
 
 final Color chipColor = Color(0xFFFFEEBF);
 
+final Color blueColor=Color(0xFF84CBFF);
+final Color redColor =Color(0xFFFF7C7C);
+final Color greenColor =Color(0xFF77DD77);
+
 final double pagePadding = 24;
 final double cardSpacing = 16;
 final double chipPadding = 4;
+final double buttonPadding = 8;
 
 final FontWeight lightFontWeight = FontWeight.w300;
 final FontWeight normalFontWeight = FontWeight.w600;
