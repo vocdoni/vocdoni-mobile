2874c3643cc816dd2f1f4d6a6e31a57a2863f1aa
Xavi <xavings@gmail.com>
2019-05-22 19:15:52 +0200

    WIP organization details dynamic title

--- lib/views/organization-info.dart
+++ lib/views/organization-info.dart
@@ -1,7 +1,9 @@
 import "package:flutter/material.dart";
 import 'package:vocdoni/constants/colors.dart';
+import 'package:vocdoni/constants/colors.dart' as prefix0;
 import 'package:vocdoni/modals/web-action.dart';
 import 'package:vocdoni/util/singletons.dart';
+import 'package:vocdoni/widgets/baseButton.dart';
 import 'package:vocdoni/widgets/listItem.dart';
 import 'package:vocdoni/widgets/pageTitle.dart';
 import 'package:vocdoni/widgets/section.dart';
@@ -27,33 +29,72 @@ class OrganizationInfo extends StatelessWidget {
       }
     }
 
+    double height = 200;
+    double zoneHeight = 100;
+    double pos = 0;
+    double opacity = 0;
+
     return Scaffold(
-      appBar: TopNavigation(
-        title: "",
-      ),
-      backgroundColor: baseBackgroundColor,
-      body: ListView(
-        children: <Widget>[
-          PageTitle(
-            title: organization.name,
-            subtitle: organization.entityId,
+        backgroundColor: baseBackgroundColor,
+        body: CustomScrollView(controller: ScrollController(), slivers: [
+          SliverAppBar(
+              floating: true,
+              snap: true,
+              pinned: true,
+              elevation: 0,
+              //title: Text('SliverAppBar'),
+              backgroundColor: baseBackgroundColor,
+              expandedHeight: height,
+              flexibleSpace: LayoutBuilder(
+                  builder: (BuildContext context, BoxConstraints constraints) {
+                pos = constraints.biggest.height;
+                // print('constraints=' + constraints.toString());
+
+                opacity = (pos / zoneHeight) < 1 ? pos / height : 1;
+
+                return FlexibleSpaceBar(
+                    centerTitle: true,
+                    title: Text(
+                      organization.name,
+                      style: TextStyle(color: titleColor.withOpacity(opacity)),
+                    ),
+                    background: Image.network(
+                      "https://images.unsplash.com/photo-1542601098-3adb3baeb1ec?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=5bb9a9747954cdd6eabe54e3688a407e&auto=format&fit=crop&w=500&q=60",
+                      fit: BoxFit.cover,
+                    ));
+              })
+              //flexibleSpace: FlexibleSpaceBar(
+              //  background:Image.network(organization.avatar)
+              //),
+              ),
+          SliverFixedExtentList(
+            itemExtent: 150.0,
+            delegate: SliverChildListDelegate([
+              PageTitle(
+                title: organization.name,
+                subtitle: organization.entityId,
+                titleColor: titleColor.withOpacity(1.0 - opacity),
+              ),
+              Section(text: "Description"),
+              Summary(
+                text: organization.description[organization.languages[0]],
+                maxLines: 5,
+              ),
+              Section(text: "Actions"),
+              ListItem(
+                text: "Activity",
+                onTap: () {
+                  Navigator.pushNamed(context, "/organizations/activity",
+                      arguments: organization);
+                },
+              ),
+              alreadySubscribed
+                  ? buildAlreadySubscribed(
+                      context, organization) // CUSTOM ACTIONS
+                  : buildSubscriptionTiles(context, organization) // SUBSCRIBE
+            ]),
           ),
-          Section(text: "Description"),
-          Summary(text: organization.description[organization.languages[0]], maxLines: 5,),
-          Section(text: "Actions"),
-          ListItem(
-            text: "Activity",
-            onTap: () {
-              Navigator.pushNamed(context, "/organizations/activity",
-                  arguments: organization);
-            },
-          ),
-          alreadySubscribed
-              ? buildAlreadySubscribed(context, organization) // CUSTOM ACTIONS
-              : buildSubscriptionTiles(context, organization) // SUBSCRIBE
-        ],
-      ),
-    );
+        ]));
   }
 
   /// NO ORGANIZATION
@@ -151,5 +192,3 @@ class OrganizationInfo extends StatelessWidget {
     Navigator.pop(ctx, false);
   }
 }
-
-
