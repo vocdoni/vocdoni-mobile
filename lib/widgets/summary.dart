803e0ff4101a41f0492d979b144dc2524f1af33c
Xavi <xavings@gmail.com>
2019-05-24 11:10:14 +0200

    Making summary expandable on tap

--- lib/widgets/summary.dart
+++ lib/widgets/summary.dart
@@ -1,25 +1,41 @@
 import "package:flutter/material.dart";
 import 'package:vocdoni/constants/colors.dart';
 
-class Summary extends StatelessWidget {
+class Summary extends StatefulWidget {
   final String text;
   final int maxLines;
 
   Summary({this.text, this.maxLines});
 
   @override
+  _SummaryState createState() => _SummaryState();
+}
+
+class _SummaryState extends State<Summary> with TickerProviderStateMixin {
+  bool collapsed = true;
+  @override
   Widget build(context) {
-    return Container(
-        padding: new EdgeInsets.all(pagePadding),
-        child: Text(
-          text,
-          overflow: TextOverflow.ellipsis,
-          textAlign: TextAlign.left,
-          maxLines: maxLines,
-          style: TextStyle(
-              fontSize: 16,
-              color: descriptionColor,
-              fontWeight: lightFontWeight),
-        ));
+    return AnimatedSize(
+        alignment: Alignment.topLeft,
+        curve: Curves.easeOutCubic,
+        duration: Duration(milliseconds: 300),
+        vsync: this,
+        child: InkWell(
+            onTap: () => setState(() {
+              debugPrint("is collapsed"+collapsed.toString());
+                  collapsed = !collapsed;
+                }),
+            child: Container(
+                padding: new EdgeInsets.all(pagePadding),
+                child: Text(
+                  widget.text,
+                  overflow: TextOverflow.ellipsis,
+                  textAlign: TextAlign.left,
+                  maxLines: collapsed ? widget.maxLines : 100,
+                  style: TextStyle(
+                      fontSize: 16,
+                      color: descriptionColor,
+                      fontWeight: lightFontWeight),
+                ))));
   }
 }
