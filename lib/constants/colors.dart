8f2ee10e9c23c9ddf68c310e31f652f48b680485
Xavi <xavings@gmail.com>
2019-08-03 21:12:12 +0200

    Unused stuff cleanup

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -2,34 +2,22 @@ import 'package:flutter/material.dart';
 
 
 final double opacitySecondaryElement = 0.7;
-final double opacityBackgroundColor = 0.15;
+final double opacityDecorationLines = 0.1;
+final double opacityBackgroundColor = 0.1;
 final double opacityDisabled = 0.4;
-
-final Color colorMainBackground = Color(0xFF548CFF);
-final Color colorAltBackground = Color(0xFFE4534D);
-final Color colorAlt2Background = Color(0xFFFF682D);
-
-final Color colorMainText = Color(0xFFFFFFFF); // white
-
-final colorSuccess = Color(0xFF25bd5b);
-final colorDanger = colorAltBackground;
-
-final Color colorBaseBackground = Color(0xFFF3F0ED);
-final Color colorDescription = Color(0xFF444444);
-final Color colorGuide = colorDescription.withOpacity(opacitySecondaryElement);
-final Color colorLightGuide = Color(0x66888888);
-final Color colorTitle = Color(0xFF000000);
-
-
-final Color colorChip = Color(0xFFFFEEBF);
 
 final Color colorRed = Color(0xFFFF7C7C);
 final Color colorOrange = Color(0xFFFFA800);
 final Color colorGreen = Color(0xFF66DD55);
 final Color colorBlue = Color(0xFF66BBEF);
 
+final Color colorBaseBackground = Color(0xFFF3F0ED);
+final Color colorDescription = Color(0xFF444444);
+final Color colorGuide = colorDescription.withOpacity(opacitySecondaryElement);
+final Color colorLightGuide = colorDescription.withOpacity(opacityDecorationLines);
 
-
+final Color colorTitle = Color(0xFF000000);
+final Color colorChip = Color(0xFFFFEEBF);
 final Color colorLink = colorBlue;
 
 final double paddingPage = 24;
