b3e7fc72a4b7bc8e1738b127da21eb6e59fcc284
Xavi <xavings@gmail.com>
2019-08-27 12:56:34 +0200

    Process storage persistence

--- lib/data/processMetadataBloc.dart
+++ lib/data/processMetadataBloc.dart
@@ -1,7 +1,8 @@
+import 'dart:io';
 import 'package:dvote/dvote.dart';
+import 'package:flutter/rendering.dart';
 import 'package:vocdoni/data/genericBloc.dart';
 import 'package:vocdoni/util/singletons.dart';
-
 
 class ProccessMetadataBloc extends GenericBloc<List<ProcessMetadata>> {
   final String _storageFile = PROCESSES_STORE_FILE;
@@ -15,57 +16,73 @@ class ProccessMetadataBloc extends GenericBloc<List<ProcessMetadata>> {
   /// Read and construct the data structures
   @override
   Future<void> restore() async {
-
     state.add([]);
-    // File fd;
-    // ProcessesStore store;
+    File fd;
+    ProcessMetadataStore store;
 
-    // try {
-    //   fd = File("${storageDir.path}/$_storageFile");
-    //   if (!(await fd.exists())) {
-    //     return;
-    //   }
-    // } catch (err) {
-    //   print(err);
-    //   throw BlocRestoreError(
-    //       "There was an error while accessing the local data");
-    // }
+    try {
+      fd = File("${storageDir.path}/$_storageFile");
+      if (!(await fd.exists())) {
+        return;
+      }
+    } catch (err) {
+      print(err);
+      throw BlocRestoreError(
+          "There was an error while accessing the local data");
+    }
 
-    // try {
-    //   final bytes = await fd.readAsBytes();
-    //   store = ProcessesStore.fromBuffer(bytes);
-    //   state.add(store.items);
-    // } catch (err) {
-    //   print(err);
-    //   throw BlocRestoreError(
-    //       "There was an error while processing the local data");
-    // }
+    try {
+      final bytes = await fd.readAsBytes();
+      store = ProcessMetadataStore.fromBuffer(bytes);
+      state.add(store.items);
+    } catch (err) {
+      print(err);
+      throw BlocRestoreError(
+          "There was an error while processing the local data");
+    }
   }
 
   @override
-  Future<void> persist() {
-    print("Unimplemented: processes > persist()");
-    // TODO:
-    throw BlocPersistError("There was an error while storing the changes");
+  Future<void> persist() async {
+    try {
+      File fd = File("${storageDir.path}/$_storageFile");
+      ProcessMetadataStore store = ProcessMetadataStore();
+      store.items.addAll(state.value);
+      await fd.writeAsBytes(store.writeToBuffer());
+    } catch (err) {
+      print(err);
+      throw BlocPersistError("There was an error while storing the changes");
+    }
   }
 
   /// Sets the given value as the current one and persists the new data
   @override
   Future<void> set(List<ProcessMetadata> data) async {
     super.set(data);
-    //await persist();
+    await persist();
   }
 
-  // CUSTOM OPERATIONS
+  Future<void> add(ProcessMetadata processMetadata, String processId,
+      String entityId) async {
+    if (!(processMetadata is ProcessMetadata))
+      throw FlutterError("The entity parameter is invalid");
 
- Future< ProcessMetadata> get(String  processIds) async {
+    processMetadata.meta[META_PROCESS_ID] = processId;
+    processMetadata.meta[META_ENTITY_ID] = entityId;
 
-   return processesBloc.value[0];
+    final currentIndex =
+        value.indexWhere((e) => e.meta[META_PROCESS_ID] == processId);
+    // Already exists
+    if (currentIndex >= 0) {
+      final currentProcessess = value;
+      currentProcessess[currentIndex] = processMetadata;
+      await set(currentProcessess);
+    } else {
+      value.add(processMetadata);
+      await set(value);
+    }
+
+    // CUSTOM OPERATIONS
+
   }
-
 }
-
-// TODO: Use a protobuf model
-class Process {}
-
-class ProcessesStore {}
