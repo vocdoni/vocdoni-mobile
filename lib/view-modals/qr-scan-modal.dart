0f17cbce0aaa05faa1309ea1de23350eb15dd013
Jør∂¡ <itsbrickpop@gmail.com>
2020-03-18 17:09:54 +0100

    Prevent QR scanner black screen

--- lib/view-modals/qr-scan-modal.dart
+++ lib/view-modals/qr-scan-modal.dart
@@ -1,8 +1,10 @@
+import 'package:dvote_common/widgets/loading-spinner.dart';
 import "package:flutter/material.dart";
 import 'package:qr_mobile_vision/qr_camera.dart';
 import 'package:qr_mobile_vision/qr_mobile_vision.dart';
 import 'package:dvote_common/widgets/topNavigation.dart';
 import 'package:vocdoni/lib/singletons.dart';
+import 'package:vocdoni/lib/extensions.dart';
 
 class QrScanModal extends StatefulWidget {
   @override
@@ -27,7 +29,7 @@ class _QrScanModalState extends State<QrScanModal> {
         onBackButton: onCancel,
       ),
       body: Builder(builder: (_) {
-        if (!scanning) return Container();
+        if (!scanning) return _buildLoading(context);
 
         return Container(
             child: QrCamera(
@@ -49,7 +51,7 @@ class _QrScanModalState extends State<QrScanModal> {
 
   onScan(String value) {
     this.setState(() => scanning = false);
-    Future.delayed(Duration(milliseconds: 50))
+    Future.delayed(Duration(milliseconds: 5))
         .then((_) => Navigator.of(context).pop(value));
   }
 
@@ -58,6 +60,17 @@ class _QrScanModalState extends State<QrScanModal> {
       child: Center(
         child: Text(message ?? "The camera is not available"),
       ),
-    );
+    ).withBottomPadding(100);
+  }
+
+  Widget _buildLoading(BuildContext context) {
+    return Center(
+        child: Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text("Analyzing the code..."),
+        LoadingSpinner().withLeftPadding(30),
+      ],
+    )).withBottomPadding(100);
   }
 }
