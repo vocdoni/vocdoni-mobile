364bab6cc07f87d7f78c82abeb1995b0d912f1a4
NateWilliams2 <Williams2@grinnell.edu>
2021-02-15 14:53:03 -0600

    Don't use legacy entity/feed/process/bootnode data

--- lib/constants/storage-names.dart
+++ lib/constants/storage-names.dart
@@ -1,10 +1,13 @@
 const IDENTITIES_STORE_FILE = "identities.dat";
-const ENTITIES_STORE_FILE = "entities.dat";
+const ENTITIES_STORE_FILE = "entitiesv2.dat";
 const BOOTNODES_STORE_FILE = "bootnodes.dat";
 const PROCESSES_STORE_FILE = "processesv2.dat";
-const NEWSFEED_STORE_FILE = "feed.dat";
-const SETTINGS_STORE_FILE = "bootnodes.json";
+const NEWSFEED_STORE_FILE = "feedv2.dat";
+const SETTINGS_STORE_FILE = "bootnodesv2.json";
 const LOG_STORE_FILE = "session-logs.txt";
 
 // File containing legacy processes which cannot be decoded along with new processes
 const OLD_PROCESSES_STORE_FILE = "processes.dat";
+const OLD_NEWSFEED_STORE_FILE = "feed.dat";
+const OLD_BOOTNODES_STORE_FILE = "bootnodes.dat";
+const OLD_ENTITIES_STORE_FILE = "entities.dat";
